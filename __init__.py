7af7f301618e4c742a5d1299e9e017521a670e2f
Yizhak <ydcohen@mail.csuchico.edu>
2023-07-19 18:04:51 -0700

    modified gitignore

--- __init__.py
+++ __init__.py
@@ -10,6 +10,7 @@ import os
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel
+from flask import request
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -48,5 +49,9 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+    @babel.localeselector
+    def get_locale():
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 from app import routes, models, errors
